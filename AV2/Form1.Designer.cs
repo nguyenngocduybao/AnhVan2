d91784e86ac72684a9e989a382c320db8e55b2b1
Bảo Duy <duybao05123@gmail.com>
2018-03-07 00:58:19 +0700

    Update Label ver 3

--- AV2/Form1.Designer.cs
+++ AV2/Form1.Designer.cs
@@ -37,6 +37,28 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
+            this.label25 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
+            this.label27 = new System.Windows.Forms.Label();
+            this.label28 = new System.Windows.Forms.Label();
+            this.label29 = new System.Windows.Forms.Label();
+            this.label30 = new System.Windows.Forms.Label();
+            this.label31 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -132,18 +154,279 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.Red;
-            this.label9.Location = new System.Drawing.Point(1163, 231);
+            this.label9.Location = new System.Drawing.Point(1153, 231);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(126, 29);
             this.label9.TabIndex = 8;
             this.label9.Text = ".......(4).......";
             this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(1276, 231);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(369, 29);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "various doors to the opportunities";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(100, 302);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(1118, 29);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "of achieving better prospects in life so promotes career growth. Many awareness p" +
+    "rogrammes has been";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.Red;
+            this.label12.Location = new System.Drawing.Point(1229, 302);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(126, 29);
+            this.label12.TabIndex = 11;
+            this.label12.Text = ".......(5).......";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(1361, 302);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(235, 29);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "by the government in";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.Red;
+            this.label14.Location = new System.Drawing.Point(105, 368);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(114, 29);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "......(6)......";
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(225, 368);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(1451, 29);
+            this.label15.TabIndex = 14;
+            this.label15.Text = "the value of education in rural areas.  It brings feeling of equality among all p" +
+    "eople in the society and promotes growth and development";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(100, 428);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(161, 29);
+            this.label16.TabIndex = 15;
+            this.label16.Text = "of the country.";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(105, 513);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(120, 29);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "Education";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.ForeColor = System.Drawing.Color.Red;
+            this.label18.Location = new System.Drawing.Point(231, 513);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(120, 29);
+            this.label18.TabIndex = 17;
+            this.label18.Text = "......(7).......";
+            this.label18.Click += new System.EventHandler(this.label18_Click);
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label19.Location = new System.Drawing.Point(357, 513);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(960, 29);
+            this.label19.TabIndex = 18;
+            this.label19.Text = "a paramount role in the modern technological world. Now-a-days, there are many wa" +
+    "ys to";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.ForeColor = System.Drawing.Color.Red;
+            this.label20.Location = new System.Drawing.Point(1323, 513);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(114, 29);
+            this.label20.TabIndex = 19;
+            this.label20.Text = "......(8)......";
+            this.label20.Click += new System.EventHandler(this.label20_Click);
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.Location = new System.Drawing.Point(1443, 513);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(220, 29);
+            this.label21.TabIndex = 20;
+            this.label21.Text = "the education level.";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.Location = new System.Drawing.Point(105, 568);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(771, 29);
+            this.label22.TabIndex = 21;
+            this.label22.Text = "The whole criteria of education have been changed now. We can study ";
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label23.ForeColor = System.Drawing.Color.Red;
+            this.label23.Location = new System.Drawing.Point(882, 568);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(132, 29);
+            this.label23.TabIndex = 22;
+            this.label23.Text = ".......(9)........";
+            this.label23.Click += new System.EventHandler(this.label23_Click);
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label24.Location = new System.Drawing.Point(1020, 568);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(620, 29);
+            this.label24.TabIndex = 23;
+            this.label24.Text = "the distance learning programmes after the 12th standard";
+            // 
+            // label25
+            // 
+            this.label25.AutoSize = true;
+            this.label25.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label25.Location = new System.Drawing.Point(110, 618);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(450, 29);
+            this.label25.TabIndex = 24;
+            this.label25.Text = "together with the job. Education is not so ";
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label26.ForeColor = System.Drawing.Color.Red;
+            this.label26.Location = new System.Drawing.Point(562, 618);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(121, 29);
+            this.label26.TabIndex = 25;
+            this.label26.Text = ".....(10)......";
+            this.label26.Click += new System.EventHandler(this.label26_Click);
+            // 
+            // label27
+            // 
+            this.label27.AutoSize = true;
+            this.label27.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label27.Location = new System.Drawing.Point(689, 618);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(976, 29);
+            this.label27.TabIndex = 26;
+            this.label27.Text = ", anyone one with less money may study continuously. We can get admission in the " +
+    "big and";
+            // 
+            // label28
+            // 
+            this.label28.AutoSize = true;
+            this.label28.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label28.Location = new System.Drawing.Point(105, 671);
+            this.label28.Name = "label28";
+            this.label28.Size = new System.Drawing.Size(271, 29);
+            this.label28.TabIndex = 27;
+            this.label28.Text = "popular universities with";
+            // 
+            // label29
+            // 
+            this.label29.AutoSize = true;
+            this.label29.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label29.ForeColor = System.Drawing.Color.Red;
+            this.label29.Location = new System.Drawing.Point(382, 671);
+            this.label29.Name = "label29";
+            this.label29.Size = new System.Drawing.Size(127, 29);
+            this.label29.TabIndex = 28;
+            this.label29.Text = "......(11)......";
+            this.label29.Click += new System.EventHandler(this.label29_Click);
+            // 
+            // label30
+            // 
+            this.label30.AutoSize = true;
+            this.label30.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label30.Location = new System.Drawing.Point(515, 671);
+            this.label30.Name = "label30";
+            this.label30.Size = new System.Drawing.Size(1163, 29);
+            this.label30.TabIndex = 29;
+            this.label30.Text = "fees through the distance learning. Other small training institutes are providing" +
+    " education to enhance the skill";
+            // 
+            // label31
+            // 
+            this.label31.AutoSize = true;
+            this.label31.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label31.Location = new System.Drawing.Point(100, 723);
+            this.label31.Name = "label31";
+            this.label31.Size = new System.Drawing.Size(258, 29);
+            this.label31.TabIndex = 30;
+            this.label31.Text = " level in particular field.";
+            // 
             // AV2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1688, 1057);
+            this.Controls.Add(this.label31);
+            this.Controls.Add(this.label30);
+            this.Controls.Add(this.label29);
+            this.Controls.Add(this.label28);
+            this.Controls.Add(this.label27);
+            this.Controls.Add(this.label26);
+            this.Controls.Add(this.label25);
+            this.Controls.Add(this.label24);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -171,6 +454,28 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.Label label25;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.Label label27;
+        private System.Windows.Forms.Label label28;
+        private System.Windows.Forms.Label label29;
+        private System.Windows.Forms.Label label30;
+        private System.Windows.Forms.Label label31;
     }
 }
 
--- AV2/Form1.cs
+++ AV2/Form1.cs
@@ -36,5 +36,41 @@ namespace AV2
         {
             label9.Text = "opens";
         }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            label12.Text = "run";
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+            label14.Text = "enhancing";
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+            label18.Text = "plays";
+        }
+
+        private void label20_Click(object sender, EventArgs e)
+        {
+            label20.Text = "enhance";
+        }
+
+        private void label23_Click(object sender, EventArgs e)
+        {
+            label23.Text = "through";
+        }
+
+        private void label26_Click(object sender, EventArgs e)
+        {
+            label26.Text = "costly";
+        }
+
+        private void label29_Click(object sender, EventArgs e)
+        {
+            label29.Text = "fewer";
+        }
+
     }
 }
